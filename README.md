2681467a9b35182c4dc0ef8292816175a6993546
sidincredible <73731930+sidincredible@users.noreply.github.com>
2024-10-22 12:01:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hacktober-23 
+This is a training repository for #hacktoberfest. Contribute here for my project
---
+++ add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum,diff;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2; 
+    diff = number1 - number2;
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    
+    printf("%d - %d = %d", number1, number2, diff );
+    return 0;
+        printf("%d + %d = %d", number1, number2, sum);
+    
+    printf("%d - %d = %d", number1, number2, diff );
+    return 0;
+        printf("%d + %d = %d", number1, number2, sum);
+    
+    printf("%d - %d = %d", number1, number2, diff );
+    return 0;
+    
+    int main()
+{
+    int a = 1;
+    while ( a <= 10 )
+    {
+        printf ( "Hello World\n" );
+        a ++;
+        printf ( "Hello World\n" );
+        a ++;
+        printf ( "Hello World\n" );
+        a ++;
+        printf ( "Hello World\n" );
+        a ++;
+    }
+    return 0;
+}
+}
